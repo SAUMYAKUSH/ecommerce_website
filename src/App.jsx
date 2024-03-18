a9404a62a705a5b8acff9ee06e80c5ea5e8ae3d1
SAUMYAKUSH <saumyakushwaha803@gmail.com>
2024-03-18 10:05:33 +0530

    initialised the project using vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { productsArr } from './assets/ProductList'
+
+function App() {
+  return (
+    <div>
+    <productsArr/>
+    </div>
+  )
+}
+
+export default App
